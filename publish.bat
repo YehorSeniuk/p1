fb1a7989d07949c93df59ac823fdfe7412319218
YehorSeniuk <103454023+YehorSeniuk@users.noreply.github.com>
2023-04-20 20:24:26 +0300

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -2,5 +2,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/edu-db/p1.git master:gh-pages
+git push -f https://github.com/YehorSeniuk/p1.git master:gh-pages
 cd ..
